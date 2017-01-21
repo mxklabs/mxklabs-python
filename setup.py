7ab49e4f14fe5327cdb8af460feb2f58e66cbfc3
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-01-21 20:00:41 +0000

    Added setup files.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pydimacs',
+  packages = ['pydimcas'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A library for reading files in the DIMACS format',
+  author = 'Mark Kattenbelt',
+  author_email = 'mark.kattenbelt@gmail.com',
+  url = 'https://github.com/mxklabs/pydimacs', 
+  download_url = 'https://github.com/mxklabs/pydimacs/tarball/0.1', # todo
+  keywords = ['DIMACS', 'SAT', 'satisfiability', 'CNF'],
+  classifiers = [],
+)
