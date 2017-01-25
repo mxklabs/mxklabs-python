42f62ef313117d3cb9f629b7d6ad6891bdf8045b
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-01-25 20:45:26 +0000

    Changed the setup.py script to match the new project layout.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'pydimacs',
-  packages = ['pydimacs'], # this must be the same as the name above
+  name = 'mxklabs',
+  packages = ['mxklabs'], # this must be the same as the name above
   version = '0.1',
-  description = 'A library for reading files in the DIMACS format',
+  description = 'A python library for reading files in the DIMACS format',
   author = 'Mark Kattenbelt',
   author_email = 'mark.kattenbelt@gmail.com',
-  url = 'https://github.com/mxklabs/pydimacs', 
-  download_url = 'https://github.com/mxklabs/pydimacs/tarball/0.1', # todo
+  url = 'https://github.com/mxklabs/mxklabs-python',
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.1', # todo
   keywords = ['DIMACS', 'SAT', 'satisfiability', 'CNF'],
   classifiers = [],
 )
