5e443a428cb13afd70c037224a934d2aa887afac
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-11-12 19:04:24 +0000

    Bugfix.

--- mxklabs/test/expr/__init__.py
+++ mxklabs/test/expr/__init__.py
@@ -2,4 +2,3 @@ from mxklabs.test.expr.test_exprtype import *
 from mxklabs.test.expr.test_expr import *
 from mxklabs.test.expr.test_expranalyse import *
 from mxklabs.test.expr.test_exprbitvector import *
-from mxklabs.test.expr.test_exprwalker import *
