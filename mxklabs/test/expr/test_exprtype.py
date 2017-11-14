3b341b660c40b5a9eb053a60b335c9d5783d543c
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-11-14 21:46:30 +0000

    Added type repository test.

--- mxklabs/test/expr/test_exprtype.py
+++ mxklabs/test/expr/test_exprtype.py
@@ -5,7 +5,7 @@ import mxklabs as mxk
 
 class Test_ExprType(unittest.TestCase):
 
-  def test_exprtype_bool(self):   
+  def test_expr_type_bool(self):
     T = mxk.Bool()
     self.assertEqual("bool", str(T))
     self.assertEqual([False, True], [value.user_value() for value in T.values()])
@@ -26,7 +26,7 @@ class Test_ExprType(unittest.TestCase):
   #    (mxk.BitVector.int_to_value(2, 3), (True,))]), set([value for value in T.values()]))
   #  self.assertEqual(8, T.num_values())
     
-  def test_exprtype_functions(self):
+  def test_expr_type_functions(self):
     
     # For all classes, exprtype_type, that derive from mxk.ExprType...
     for exprtype_type in mxk.Utils.get_derived_classes(mxk, mxk.ExprType):
@@ -35,3 +35,8 @@ class Test_ExprType(unittest.TestCase):
       self.assertTrue(mxk.Utils.class_has_function(exprtype_type, 'littup_value_to_user_value'))
       self.assertTrue(mxk.Utils.class_has_function(exprtype_type, 'is_valid_user_value'))
       self.assertTrue(mxk.Utils.class_has_function(exprtype_type, 'is_valid_littup_value'))
+
+class Test_ExprTypeRepository(unittest.TestCase):
+
+  def test_expr_type_repository(self):
+    self.assertTrue(isinstance(mxk.ExprTypeRepository.get_expr_type_from_type_str('bool'), mxk.Bool))
