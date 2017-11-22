1768dc621bfcaf384dac3605510617e6d02e8920
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-11-22 20:23:43 +0000

    Moved ExpressionEvaluator to expreval.py

--- mxklabs/expr/expranalyse.py
+++ mxklabs/expr/expranalyse.py
@@ -131,35 +131,6 @@ class ConstProp(ExprWalker):
   
 ''' Evaluate the value of an expressions. '''
 
-class ExpressionEvaluator(ExprWalker):
-
-  def _process(self, expr, variable_value_map):
-    return self.bottom_up_walk(expr=expr, args=variable_value_map)
-  
-  def visit_const(self, expr, res, args):
-    return expr.value()
-    
-  def visit_var(self, expr, res, args):
-    return args[expr]
-  
-  def visit_logical_and(self, expr, res, args):
-    return et.ExprValue(expr_type=et.ExprTypeRepository._BOOL, user_value=all([res[child].user_value() for child in expr.children()]))
-  
-  def visit_logical_or(self, expr, res, args):
-    return et.ExprValue(expr_type=et.ExprTypeRepository._BOOL, user_value=any([res[child].user_value() for child in expr.children()]))
-
-  def visit_logical_not(self, expr, res, args):
-    return et.ExprValue(expr_type='bool', user_value=not res[expr.child()].user_value())
-
-  def visit_equals(self, expr, res, args):
-    return et.ExprValue(expr_type='bool', user_value=(res[child(0)] == res[child(1)]))
-  
-  ''' Quick version (avoid creating VarHarvester). '''
-  @staticmethod
-  def process(expr, variable_value_map):
-    
-    ee = ExpressionEvaluator()
-    return ee._process(expr, variable_value_map)
 
   
   
