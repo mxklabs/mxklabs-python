a7727cde0a6c320f2014a5ac0de97333a4ac4e1a
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-11-22 20:48:45 +0000

    Moved ExpressionEvaluator tests to test_expreval.py

--- mxklabs/test/expr/__init__.py
+++ mxklabs/test/expr/__init__.py
@@ -3,4 +3,5 @@ from mxklabs.test.expr.test_expranalyse import *
 from mxklabs.test.expr.test_exprbitvector import *
 from mxklabs.test.expr.test_exprcomp import *
 from mxklabs.test.expr.test_exprcond import *
+from mxklabs.test.expr.test_expreval import *
 from mxklabs.test.expr.test_exprtype import *
--- mxklabs/test/expr/test_expranalyse.py
+++ mxklabs/test/expr/test_expranalyse.py
@@ -137,15 +137,3 @@ class Test_ConstProp(unittest.TestCase):
     self._TEST(mxk.Equals(c1_, v1_), mxk.Equals(c1_, v1_))
     self._TEST(mxk.Const('bool', False), mxk.Equals(c1_, c2_))
     self._TEST(mxk.Const('bool', True), mxk.Equals(c1_, c3_))
-
-class Test_ExpressionEvaluator(unittest.TestCase):
-  
-  def test_expression_evaluator_and(self):
-
-    v1_ = mxk.Var(expr_type='bool', id="v1")
-    e_ = mxk.LogicalAnd(
-      mxk.Var(expr_type='bool', id="v1"),
-      mxk.Const(expr_type='bool', user_value=True))
-
-    self.assertTrue(mxk.ExpressionEvaluator.process(e_, {v1_: mxk.ExprValue(expr_type=mxk.ExprTypeRepository._BOOL, user_value=True)}).user_value())
-    self.assertFalse(mxk.ExpressionEvaluator.process(e_, {v1_: mxk.ExprValue(expr_type=mxk.ExprTypeRepository._BOOL, user_value=False)}).user_value())
