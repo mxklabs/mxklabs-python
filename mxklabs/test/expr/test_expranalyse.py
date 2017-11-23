4fcca0cdd735bad486191d56b2c2c1c7a811555d
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2017-11-23 21:55:39 +0000

    Added further tests for ExprEvaluator.

--- mxklabs/test/expr/test_expranalyse.py
+++ mxklabs/test/expr/test_expranalyse.py
@@ -49,6 +49,23 @@ class Test_ExprAnalyse(unittest.TestCase):
             "expected '{}' to have a function '{}'".format(
               visitor_type.__name__, visit_method_name))
 
+  def test_expranalysis_expr_visitor_functions(self):
+
+    # For all classes, visitor_type, that derive from mxk.ExprWalker...
+    for visitor_type in mxk.Utils.get_derived_classes(mxk,
+                                                      mxk.ExprVisitor):
+      # For all classes, expr_type, that derive from mxk.Expr...
+      for expr_type in mxk.Utils.get_derived_classes(mxk, mxk.Expr):
+        # Test that visitor_type handles expr_type.
+        visit_method_name = '_visit_' + mxk.Utils.camel_case_to_snake_case(
+          expr_type.__name__)
+
+        self.assertTrue(
+          mxk.utils.Utils.class_has_function(visitor_type,
+                                             visit_method_name),
+          "expected '{}' to have a function '{}'".format(
+            visitor_type.__name__, visit_method_name))
+
 
 class Test_VarHarvester(unittest.TestCase):
   
--- mxklabs/test/expr/test_expreval.py
+++ mxklabs/test/expr/test_expreval.py
@@ -19,7 +19,28 @@ class Test_ExprEvaluator(unittest.TestCase):
         act_result = mxk.ExprEvaluator.eval(expr, variable_assignment)
         self.assertEqual(exp_result, act_result)
 
-    def test_expression_evaluator_and(self):
+    def test_expreval_const(self):
+        c1_ = mxk.Const('bool', True)
+        c2_ = mxk.Const('bool', False)
+        true_ = mxk.ExprValue('bool', True)
+        false_ = mxk.ExprValue('bool', False)
+
+        self._TEST(true_, c1_, {})
+        self._TEST(false_, c2_, {})
+
+    def test_expreval_var(self):
+        v1_ = mxk.Var('bool', 'v1')
+        v2_ = mxk.Var('uint8', 'v2')
+
+        true_ = mxk.ExprValue('bool', True)
+        false_ = mxk.ExprValue('bool', False)
+        eight_ = mxk.ExprValue('uint8', 8)
+
+        self._TEST(true_, v1_, {v1_: true_})
+        self._TEST(false_, v1_, {v1_: false_})
+        self._TEST(eight_, v2_, {v2_: eight_})
+
+    def test_expreval_logical_and(self):
         v1_ = mxk.Var('bool', "v1")
         v2_ = mxk.Var('bool', "v2")
         true_ = mxk.ExprValue('bool', True)
@@ -31,3 +52,41 @@ class Test_ExprEvaluator(unittest.TestCase):
         self._TEST(false_, e_, {v1_: true_, v2_: false_})
         self._TEST(false_, e_, {v1_: false_, v2_: true_})
         self._TEST(false_, e_, {v1_: false_, v2_: false_})
+
+    def test_expreval_logical_or(self):
+
+        v1_ = mxk.Var('bool', "v1")
+        v2_ = mxk.Var('bool', "v2")
+        true_ = mxk.ExprValue('bool', True)
+        false_ = mxk.ExprValue('bool', False)
+
+        e_ = mxk.LogicalOr(v1_, v2_)
+
+        self._TEST(true_, e_, {v1_: true_, v2_: true_})
+        self._TEST(true_, e_, {v1_: true_, v2_: false_})
+        self._TEST(true_, e_, {v1_: false_, v2_: true_})
+        self._TEST(false_, e_, {v1_: false_, v2_: false_})
+
+    def test_expreval_logical_not(self):
+        v1_ = mxk.Var('bool', "v1")
+        true_ = mxk.ExprValue('bool', True)
+        false_ = mxk.ExprValue('bool', False)
+
+        e_ = mxk.LogicalNot(v1_)
+
+        self._TEST(false_, e_, {v1_: true_})
+        self._TEST(true_, e_, {v1_: false_})
+
+    def test_expreval_equals(self):
+        v1_ = mxk.Var('uint8', "v1")
+        v2_ = mxk.Var('uint8', "v2")
+        true_ = mxk.ExprValue('bool', True)
+        false_ = mxk.ExprValue('bool', False)
+        int76_ = mxk.ExprValue('uint8', 76)
+        int122_ = mxk.ExprValue('uint8', 122)
+
+        e_ = mxk.Equals(v1_, v2_)
+
+        self._TEST(false_, e_, {v1_: int76_, v2_: int122_})
+        self._TEST(true_, e_, {v1_: int76_, v2_: int76_})
+
