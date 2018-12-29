b1e064778bf9a0daace498c2b33d2a319cb426e3
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2018-12-29 07:06:05 +0000

    Bumped version number to 0.0.4.

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mxklabs',
   packages = ['mxklabs'],
-  version = '0.0.3',
-  description = 'A python library for reading files in the DIMACS format',
+  version = '0.0.4',
+  description = 'A selection python code written for mxklabs projects that may be useful elsewhere.',
   author = 'Mark Kattenbelt',
   author_email = 'mark.kattenbelt@gmail.com',
   url = 'https://github.com/mxklabs/mxklabs-python',
-  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.3', # todo
-  keywords = ['DIMACS', 'SAT', 'satisfiability', 'CNF'],
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.4', # todo
+  keywords = ['mxklabs', 'mxklabs.data', 'mxklabs.sat', 'DIMACS', 'SAT',
+              'satisfiability', 'CNF', 'circular', 'buffer', 'persistent'],
   classifiers = [],
 )
