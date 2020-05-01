8141cc484a458c23ef427a4e09397cbbc10d3127
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2020-05-01 19:34:49 +0100

    Upped version to 0.0.8

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'mxklabs',
   packages = ['mxklabs', 'mxklabs.data', 'mxklabs.dimacs', 'mxklabs.rsa'],
-  version = '0.0.7',
+  version = '0.0.8',
   description = 'A selection python code written for mxklabs projects that may be useful elsewhere.',
   author = 'Mark Kattenbelt',
   author_email = 'mark.kattenbelt@gmail.com',
   url = 'https://github.com/mxklabs/mxklabs-python',
-  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.7',
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.8',
   keywords = ['mxklabs', 'mxklabs.data', 'mxklabs.sat', 'DIMACS', 'SAT',
-              'satisfiability', 'CNF', 'circular', 'buffer', 'persistent'],
+              'satisfiability', 'CNF', 'RSA', 'circular', 'buffer', 'persistent'],
   classifiers = [],
   include_package_data=True,
   install_requires=[
