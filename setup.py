e3bb0c1ea00c2c1908915aa863877637a67cbf77
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2020-05-01 19:46:49 +0100

    Bumped version, tried different mechanism for distributing asn file.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from distutils.core import setup
 setup(
   name = 'mxklabs',
   packages = ['mxklabs', 'mxklabs.data', 'mxklabs.dimacs', 'mxklabs.rsa'],
-  version = '0.0.8',
+  version = '0.0.10',
   description = 'A selection python code written for mxklabs projects that may be useful elsewhere.',
   author = 'Mark Kattenbelt',
   author_email = 'mark.kattenbelt@gmail.com',
   url = 'https://github.com/mxklabs/mxklabs-python',
-  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.8',
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.10',
   keywords = ['mxklabs', 'mxklabs.data', 'mxklabs.sat', 'DIMACS', 'SAT',
               'satisfiability', 'CNF', 'RSA', 'circular', 'buffer', 'persistent'],
   classifiers = [],
-  include_package_data=True,
+  package_data={"": ["*.asn"]},
   install_requires=[
     'asn1tools'
   ]
