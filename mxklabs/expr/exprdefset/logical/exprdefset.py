b2a3e0fb300c86500c2d73f9ca299f2d6f28246b
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2021-01-16 13:12:51 +0000

    Renamed files.

---
+++ mxklabs/expr/exprdefset/logical/exprdefset.py
@@ -0,0 +1,21 @@
+from ...exprdefset_ import ExprDefSet
+from .logical_not import LogicalNot
+from .logical_or import LogicalOr
+
+class LogicalExprDefSet(ExprDefSet):
+
+  def __init__(self, ctx):
+    ExprDefSet.__init__(self, ctx, baseid='logical', package='mxklabs.expr.exprdefset')
+    self._expr_defs = [
+      LogicalNot(ctx=ctx, expr_def_set=self),
+      LogicalOr(ctx=ctx, expr_def_set=self)
+    ]
+
+  def expr_defs(self):
+    return self._expr_defs
+
+  def valtype_ids(self):
+    return ["mxklabs.expr.valtype.bool"]
+
+  def targets(self):
+    return []
